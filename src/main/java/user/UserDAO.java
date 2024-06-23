050b7182530587122daae37b5e4c673d7e097921
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2024-06-23 19:52:03 +0900

    feat join
    making sign in feature

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -15,7 +15,7 @@ public class UserDAO {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/BBS";
 			String dbID = "root";
-			String dbPassword = "root";
+			String dbPassword = "Root7578";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
@@ -45,4 +45,22 @@ public class UserDAO {
 		}
 		return -2; // 데이터베이스 호출
 	}
+	
+	public int join(User user) {
+		String SQL = "INSERT INTO USER VALUES(?,?,?,?,?); ";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+		return -1; // 데이터 베이스 오류 시
+	}
+	
 }
