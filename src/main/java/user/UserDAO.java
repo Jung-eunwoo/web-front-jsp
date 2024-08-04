ba8394aa1c858f8f9fa6ad94897857f7cbe8a951
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2024-08-04 21:33:35 +0900

    Update UserDAO.java

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -13,9 +13,9 @@ public class UserDAO {
 
 	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/BBS";
-			String dbID = "root";
-			String dbPassword = "Root7578";
+			String dbURL = "jdbc:mysql://localhost:";
+			String dbID = "";
+			String dbPassword = "";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
